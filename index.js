ccbb6471265ea36b19af8d245192aa7d72a22333
shajmil <v.jshejmil@gmail.com>
2022-02-17 12:33:48 +0530

    public folder created and some admin files also added

--- index.js
+++ index.js
@@ -6,54 +6,23 @@ const Product = require('./models/Product');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded());
 app.use(express.json());
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'public'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/admin', (req, res) => {
+  res.render('admin/index');
+});
+
+app.get('/admin/dashboard', (req, res) => {
+  res.render('admin/dashboard');
+});
 
 const PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.render('pages/add-product');
-});
-
-app.post('/', async (req, res, next) => {
-  try {
-    const result = await Product.create(req.body);
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-app.get('/:id', async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    console.log(product);
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-app.patch('/:id', async (req, res) => {
-  try {
-    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    console.log(product);
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-app.delete('/:id', async (req, res) => {
-  try {
-    await Product.findByIdAndDelete(req.params.id);
-  } catch (err) {
-    console.log(err);
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server listenoing on port ${PORT}`);
